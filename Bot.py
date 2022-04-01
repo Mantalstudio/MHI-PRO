fbda2782a8ba027a00135cbff567bd84c76dcb61
Mantalstudio <75010285+Mantalstudio@users.noreply.github.com>
2022-04-01 13:12:44 +0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -555,7 +555,6 @@ def ceker(idf,pw):
 		print('\r%s++++ %s|%s ----> CP       %s'%(b,idf,pw,x))
 		print('\r%s---> Cant Check Option (Check Login In Lite/Mbasic)%s'%(u,x))
 		open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
-                cp+=2
 # OPSI CEKER
 def cek_opsi():
 	c = len(akun)
